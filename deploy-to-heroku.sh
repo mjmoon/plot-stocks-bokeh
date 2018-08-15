b3e810670f30c363ba55ed1ac2f94e4751c7e1b4
michael moon <michael.jongho.moon@gmail.com>
2018-08-15 14:00:06 -0400

    Update pip in docker file. Add container:release to deploy script.

--- deploy-to-heroku.sh
+++ deploy-to-heroku.sh
@@ -1,3 +1,4 @@
 docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 docker build -t registry.heroku.com/mm-plot-stocks/web ./app
 docker push registry.heroku.com/mm-plot-stocks/web
+heroku container:release web
